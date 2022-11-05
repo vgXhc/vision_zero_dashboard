ddd884c136d09eba75ccf37142d2b046c2e33819
vgXhc <kliems@gmail.com>
2022-11-05 11:33:20 -0500

    refined update_crash_data.R script (and corresponding GH action)

--- update_crash_data.R
+++ update_crash_data.R
@@ -1,5 +1,9 @@
+# script to download crash data, pre-process, and then upload to a {{pins}} board
+# script is run weekly via a Github action
+
 library(pins)
 library(sf)
+library(lubridate)
 
 board <- board_s3("vzpins", 
                   region = "us-east-1", 
@@ -7,7 +11,29 @@ board <- board_s3("vzpins",
                   secret_access_key = Sys.getenv("S3_SECRET_ACCESS_KEY"))
 
 
-download.file("https://CommunityMaps.wi.gov/crash/public/crashesKML.do?filetype=json&startyear=2017&endyear=2018&injsvr=K&injsvr=A&injsvr=B&injsvr=C&injsvr=O&county=dane", "crashes_hist.json")
-df_hist <- st_read("crashes_hist.json")
+download_crashes <- function(year) {
+  uri <- paste0("https://CommunityMaps.wi.gov/crash/public/crashesKML.do?filetype=json&startyear=", 
+                year, 
+                "&endyear=", 
+                year, 
+                "&injsvr=K&injsvr=A&injsvr=B&injsvr=C&injsvr=O&county=dane")
+  download.file(uri, "crashes_hist.json")
+  
+  df_hist <- st_read("crashes_hist.json")
+  
+  # to access the various flags in the data, we need to parse the json once more
+  # and then add that to the original crashes data frame
+  crashesJSON <- fromJSON("crashes_hist.json")
+  crashes_hist <- df_hist %>%
+    add_column(crashesJSON$features$properties$flags)
+  crashes_hist |> 
+    select(-flags)
+}
 
-board |> pin_write(df_hist, "crashes_2017", versioned = F, type = "rds")
+crashes_all_dane <- map_dfr(2017:year(today()), download_crashes)
+
+
+board |> pin_write(crashes_all_dane, "crashes_all_dane", versioned = F, type = "rds")
+
+
+
